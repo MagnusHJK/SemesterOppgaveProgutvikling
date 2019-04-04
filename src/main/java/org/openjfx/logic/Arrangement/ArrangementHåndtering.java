fbd08552320afb39ce2ddd7f95c7c6f994159665
MagnusHJK <MagnusHJK@gmail.com>
2019-04-04 14:50:45 +0200

    Lagt til lagObservableList

--- src/main/java/org/openjfx/logic/Arrangement/ArrangementHåndtering.java
+++ src/main/java/org/openjfx/logic/Arrangement/ArrangementHåndtering.java
@@ -1,9 +1,14 @@
 package org.openjfx.logic.Arrangement;
 
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static javafx.scene.input.KeyCode.T;
 
 //Klasse for håndtering av Arrangement fra GUI, lages, endres eller slettes
 public class ArrangementHåndtering {
@@ -14,4 +19,22 @@ public class ArrangementHåndtering {
         return true;
     }
 
+
+
+    //Lager en ObservableList som JavaFX kan bruke i sine komponenter
+    public ObservableList<Arrangement> lagObservableList(ArrayList<Arrangement> arrangementListe) throws Exception{
+
+        ObservableList<Arrangement> arrangementObservableListe = FXCollections.observableArrayList();
+        arrangementObservableListe.addAll(arrangementListe);
+
+        return arrangementObservableListe;
+    }
+
+
+    //Lager ArrayList
+    public List<Arrangement> lagArrayList(ObservableList<Arrangement> arrangementObservableList){
+        List<Arrangement> ArrangementList = arrangementObservableList.stream().collect(Collectors.toList());
+
+        return ArrangementList;
+    }
 }
