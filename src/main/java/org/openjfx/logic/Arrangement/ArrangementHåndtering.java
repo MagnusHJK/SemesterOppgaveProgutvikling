0eaad0fb960a56af3fa2132a9109b6f7dae3a0f5
MagnusHJK <MagnusHJK@gmail.com>
2019-04-04 12:10:57 +0200

    lagt inn metode som hører hjemme i ArrangementHåndtering

--- src/main/java/org/openjfx/logic/Arrangement/ArrangementHåndtering.java
+++ src/main/java/org/openjfx/logic/Arrangement/ArrangementHåndtering.java
@@ -1,7 +1,17 @@
 package org.openjfx.logic.Arrangement;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 //Klasse for håndtering av Arrangement fra GUI, lages, endres eller slettes
 public class ArrangementHåndtering {
+
+
+    //Endrer et spesifikt arrangement
+    public boolean endreArrangement(Arrangement arrangement){
+        return true;
+    }
 
 }
