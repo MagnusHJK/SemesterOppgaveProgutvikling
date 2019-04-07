554aca473d3e7471b187914a259e62519e2bfa3a
Jakob Olsrud Johansen <jakoboj1999@gmail.com>
2019-04-07 18:05:03 +0200

    Inputvalidering for strengene i arrangement

--- src/main/java/org/openjfx/logic/Arrangement/ArrangementValidering.java
+++ src/main/java/org/openjfx/logic/Arrangement/ArrangementValidering.java
@@ -1,7 +1,38 @@
 package org.openjfx.logic.Arrangement;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ArrangementValidering {
+
+    public static boolean valider(Arrangement arrangement) {
+        String navn = arrangement.getNavn();
+        Pattern patternNavn = Pattern.compile("^[A-ZÆØÅa-zæøå ]{2,30}$");
+        Matcher matchNavn = patternNavn.matcher(navn);
+        if(matchNavn.matches()) {
+            return true;
+        }
+        String artist = arrangement.getArtist();
+        Matcher matchArtist = patternNavn.matcher(artist);
+        if(matchArtist.matches()) {
+            return true;
+        }
+        String sted = arrangement.getSted();
+        Pattern patternSted = Pattern.compile("^[A-ZÆØÅa-zæøå ]{2,30}$");
+        Matcher matchSted = patternSted.matcher(sted);
+        if(matchSted.matches()) {
+            return true;
+        }
+        String beskrivelse = arrangement.getBeskrivelse();
+        Pattern patternBeskrivelse = Pattern.compile("^[A-ZÆØÅa-zæøå0-9 ./-]");
+        Matcher matchBeskrivelse = patternBeskrivelse.matcher(beskrivelse);
+        if(matchBeskrivelse.matches()) {
+            return true;
+        }
+        return false;
+    }
 
 }
