f09fc0ad1699432572d08e8df53b9c02071da89b
MagnusHJK <MagnusHJK@gmail.com>
2019-04-28 13:36:58 +0200

    Slett lokale metode

--- src/main/java/org/openjfx/logic/Lokale/LokaleHåndtering.java
+++ src/main/java/org/openjfx/logic/Lokale/LokaleHåndtering.java
@@ -3,6 +3,8 @@ package org.openjfx.logic.Lokale;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class LokaleHåndtering {
@@ -14,5 +16,24 @@ public class LokaleHåndtering {
         observListe.addAll(ArrayListe);
 
         return observListe;
+    }
+
+    //Sletter det inngitte lokalet fra programmet og databasen
+    //Leser array, finner lokalet som matcher og sletter, skriver endring til serialisering
+    //TODO Exceptions
+    public void slettLokale(Lokale lokale){
+        LokaleSerialiser serialiser = new LokaleSerialiser();
+
+        try{
+            ArrayList<Lokale> liste = serialiser.lesArrayFraFil();
+
+            liste.removeIf(Lokale -> Lokale.getLokaleID().equals(lokale.getLokaleID()));
+
+            serialiser.skrivArrayTilFil(liste);
+
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 }
