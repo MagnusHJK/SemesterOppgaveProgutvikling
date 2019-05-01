a8115fcbeff7c68ef35bdd8a80bbdd4a36416ca1
MagnusHJK <MagnusHJK@gmail.com>
2019-05-01 19:55:58 +0200

    La til Billett håndtering både legg til og slett

--- src/main/java/org/openjfx/logic/Billett/BillettHåndtering.java
+++ src/main/java/org/openjfx/logic/Billett/BillettHåndtering.java
@@ -3,8 +3,11 @@ package org.openjfx.logic.Billett;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.openjfx.logic.Arrangement.Arrangement;
+import org.openjfx.logic.Arrangement.ArrangementHåndtering;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.BiFunction;
 
 public class BillettHåndtering {
 
@@ -36,5 +39,64 @@ public class BillettHåndtering {
         }
 
         return plassNummer;
+    }
+
+
+    public String leggTilBillett(Arrangement arrangement, String telefonnr, int antallBilletter){
+        BillettHåndtering håndteringBill = new BillettHåndtering();
+        ArrangementHåndtering håndteringArr = new ArrangementHåndtering();
+
+        int plassnummer = 0;
+        String billettTekst = "";
+        ArrayList<Billett> billettListe;
+
+
+        for(int i = 0; i < antallBilletter; i++){
+            plassnummer = håndteringBill.finnPlassnummer(arrangement);
+
+
+            Billett billett = new Billett(arrangement, plassnummer, arrangement.getSted(),
+                    arrangement.getDato(), arrangement.getTidspunkt(), telefonnr);
+
+            arrangement.getSalg()[plassnummer] = billett;
+
+
+            billettTekst += arrangement.getSalg()[plassnummer].toString() + "\n";
+
+            //Serialiserer og legger til billettene i database
+            try{
+                BillettSerialiser serialiserBill = new BillettSerialiser();
+                billettListe = serialiserBill.lesArrayFraFil();
+
+
+                billettListe.add(billett);
+                serialiserBill.skrivArrayTilFil(billettListe);
+
+                håndteringArr.oppdaterArrangementSalg(arrangement);
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+
+
+        return billettTekst;
+    }
+
+    public void slettBillett(Billett billett){
+        BillettSerialiser serialiser = new BillettSerialiser();
+
+        try{
+            ArrayList<Billett> liste = serialiser.lesArrayFraFil();
+
+            liste.removeIf(Billett -> Billett.getPlassnummer()  == (billett.getPlassnummer()));
+
+            serialiser.skrivArrayTilFil(liste);
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 }
