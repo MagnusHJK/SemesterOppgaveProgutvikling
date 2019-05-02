a07a2d609d9eeaed69b51cd30bcf810ea4d6bb25
Martin <martin.johansen99@hotmail.com>
2019-05-02 13:45:20 +0200

    fikset skriving til fil jobj

--- src/main/java/org/openjfx/logic/Filhåndtering/skriTilJobj.java
+++ src/main/java/org/openjfx/logic/Filhåndtering/skriTilJobj.java
@@ -17,12 +17,12 @@ public class skriTilJobj extends skrivTilFil{
 
 
     @Override
-    public void LokaleTiljobj(ArrayList<Lokale> lokale, String path) {
+    public void skrivTilJobj(ArrayList data, String path) {
         try {
             FileOutputStream fileOutput = new FileOutputStream(path);
             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
 
-            objectOutput.writeObject(lokale);
+            objectOutput.writeObject(data);
 
             fileOutput.close();
             objectOutput.close();
@@ -35,56 +35,8 @@ public class skriTilJobj extends skrivTilFil{
     }
 
     @Override
-    public void ArrangementTilJobj(ArrayList<Arrangement> arrangement, String path) {
-
-        try {
-            FileOutputStream fileOutput = new FileOutputStream(path);
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
-
-            objectOutput.writeObject(arrangement);
-
-            fileOutput.close();
-            objectOutput.close();
-
-        } catch(FileNotFoundException fnf) {
-            alertbox.feil(filException.filException());
-        } catch(IOException io) {
-            alertbox.feil(inputException.ioException());
-        }
-    }
-
-    @Override
-    public void PersonTilJobj(ArrayList<Kontaktperson> person, String path) {
-
-        try {
-            FileOutputStream fileOutput = new FileOutputStream(path);
-            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
-
-            objectOutput.writeObject(person);
-
-            fileOutput.close();
-            objectOutput.close();
-
-        } catch(FileNotFoundException fnf) {
-            alertbox.feil(filException.filException());
-        } catch(IOException io) {
-            alertbox.feil(inputException.ioException());
-        }
+    public void skrivTilCsv(ArrayList data, String path) {
 
     }
 
-    @Override
-    public void LokaleTilCsv(ArrayList<Lokale> lokale, String path) {
-
-    }
-
-    @Override
-    public void ArrangementTilCsv(ArrayList<Arrangement> arrangement, String path) {
-
-    }
-
-    @Override
-    public void PersonTilCsv(ArrayList<Kontaktperson> person, String path) {
-
-    }
 }
